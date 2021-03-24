0be1f2c0dff6fe96a9a8e1b735ad3845241fa2d5
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2021-03-24 08:25:56 +0000

    Only push Docker images from CI

--- {{ cookiecutter.repo_name }}/buildkite/build-test-push.sh
+++ {{ cookiecutter.repo_name }}/buildkite/build-test-push.sh
@@ -4,11 +4,11 @@ set -eEuo pipefail
 HERE="$(dirname "${BASH_SOURCE[0]}")"
 
 if [ "${BUILDKITE:-}" ]; then
-    GIT_COMMIT=${BUILDKITE_COMMIT:0:7}
-    GIT_BRANCH=$BUILDKITE_BRANCH
+  GIT_COMMIT=${BUILDKITE_COMMIT:0:7}
+  GIT_BRANCH=$BUILDKITE_BRANCH
 else
-    GIT_COMMIT=$(git rev-parse --short=7 HEAD)
-    GIT_BRANCH=$(git symbolic-ref --short HEAD)
+  GIT_COMMIT=$(git rev-parse --short=7 HEAD)
+  GIT_BRANCH=$(git symbolic-ref --short HEAD)
 fi
 
 # Deal with dependabot tags incompatible with Docker e.g. `dependabot/npm_and_yarn/app/lodash-4.17.19`
@@ -17,9 +17,11 @@ GIT_BRANCH=${GIT_BRANCH//\//-}
 IMAGE_NAME={{ cookiecutter.docker_image }}
 
 docker build \
-    --build-arg GIT_BRANCH --build-arg GIT_COMMIT --build-arg CI \
-    --tag "$IMAGE_NAME:$GIT_BRANCH" --tag "$IMAGE_NAME:$GIT_COMMIT" \
-    "$HERE"/../src/
+  --build-arg GIT_BRANCH --build-arg GIT_COMMIT --build-arg CI \
+  --tag "$IMAGE_NAME:$GIT_BRANCH" --tag "$IMAGE_NAME:$GIT_COMMIT" \
+  "$HERE"/../src/
 
-docker push "$IMAGE_NAME:$GIT_BRANCH"
-docker push "$IMAGE_NAME:$GIT_COMMIT"
+if [ "${BUILDKITE:-}" ]; then
+  docker push "$IMAGE_NAME:$GIT_BRANCH"
+  docker push "$IMAGE_NAME:$GIT_COMMIT"
+fi
